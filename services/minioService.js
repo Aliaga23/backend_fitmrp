8ba5e6df63e98d5f7caf96cc79b724fbece8fe63
Arturo Aliaga <jaav2801@gmail.com>
2024-10-21 00:17:40 -0400

    minio conexion

--- services/minioService.js
+++ services/minioService.js
@@ -5,11 +5,24 @@ const minioClient = new Minio.Client({
   endPoint: process.env.MINIO_ENDPOINT || 'bucket-production-96ac.up.railway.app',
   port: parseInt(process.env.MINIO_PORT, 10) || 443,
   useSSL: true,  // Asegúrate de que esté en true porque estás usando HTTPS
-  accessKey: process.env.MINIO_ACCESS_KEY || '3xxAZAzlN0fUpJ4uY2NPyuMaVIAGjF5j',
-  secretKey: process.env.MINIO_SECRET_KEY || 'LIcBgbY9ryWTLy48HyRqYQ6H9ir1ExTOCre8G67WbB3CzAv',
+  accessKey: process.env.MINIO_ACCESS_KEY || 'ftyahNvc0fDokIn88yE6',
+  secretKey: process.env.MINIO_SECRET_KEY || 'zXKrdbZPtLaKVw85e2D0hd6UJTQHgvX3zeVuZ4bE',
 });
 
-const bucketName = process.env.MINIO_BUCKET || 'my-bucket';
+const bucketName = process.env.MINIO_BUCKET || 'imagenes';
+
+// Función para verificar la conexión
+const testConnection = async () => {
+  try {
+    const buckets = await minioClient.listBuckets();
+    console.log('Buckets disponibles:', buckets);
+  } catch (error) {
+    console.error('Error al conectarse a MinIO:', error);
+  }
+};
+
+// Llama a la función de prueba para verificar la conexión
+testConnection();
 
 // Función para subir archivos
 const uploadFile = async (fileName, filePath, metaData) => {
