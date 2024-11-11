cac060ce428ff8410b5f5854b854a16162992de4
Arturo Aliaga <jaav2801@gmail.com>
2024-11-11 08:51:40 -0400

    me equivoque ahora si

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const app = express();
 
 // Middleware CORS
 app.use(cors({
-  origin: 'https://backendfitmrp-production.up.railway.app',
+  origin: 'https://fitmrp.up.railway.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
