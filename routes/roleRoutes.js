383f5e3ed6fcbaf5cda038dd7cdce271a3086b7d
Arturo Aliaga <jaav2801@gmail.com>
2024-12-02 17:29:11 -0400

    Bitacora Reinicio 3

--- routes/roleRoutes.js
+++ routes/roleRoutes.js
@@ -1,22 +1,53 @@
 const express = require('express');
-const { createRole, getRoles, getRoleById, updateRole, deleteRole } = require('../controllers/roleController');
+const {
+    createRole,
+    getRoles,
+    getRoleById,
+    updateRole,
+    deleteRole,
+} = require('../controllers/roleController');
 const router = express.Router();
 const { verifyToken } = require('../middlewares/authMiddleware');
 const registrarBitacora = require('../middlewares/bitacoraMiddleware');
 
 // Ruta para crear un rol
-router.post('/',verifyToken,  registrarBitacora('Creación de un producto'), createRole);
+router.post(
+    '/',
+    verifyToken,
+    registrarBitacora('Creación de un rol'),
+    createRole
+);
 
 // Ruta para obtener todos los roles
-router.get('/', getRoles);
+router.get(
+    '/',
+    verifyToken,
+    registrarBitacora('Consulta de todos los roles'),
+    getRoles
+);
 
 // Ruta para obtener un rol por ID
-router.get('/:id', getRoleById);
+router.get(
+    '/:id',
+    verifyToken,
+    registrarBitacora('Consulta de un rol por ID'),
+    getRoleById
+);
 
 // Ruta para actualizar un rol
-router.put('/:id', updateRole);
+router.put(
+    '/:id',
+    verifyToken,
+    registrarBitacora('Actualización de un rol'),
+    updateRole
+);
 
 // Ruta para eliminar un rol
-router.delete('/:id', deleteRole);
+router.delete(
+    '/:id',
+    verifyToken,
+    registrarBitacora('Eliminación de un rol'),
+    deleteRole
+);
 
 module.exports = router;
